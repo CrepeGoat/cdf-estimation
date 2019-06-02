c2fcc7b838441d1f89d6fcff27bc193af206e819
CrepeGoat <awqatty.b@gmail.com>
2019-06-02 03:09:53 -0400

    First attempt to move over to: - optimizing the **product** of smoothness and likelihood - using scipy's `minimize` function (since the objective is no longer polynomial) - organized code

--- likelihood_funcs.py
+++ likelihood_funcs.py
@@ -1,10 +1,19 @@
+from itertools import chain
+
+
+def nCkarray(k_array):
+    result = 1
+    for i, j in enumerate(chain(*(range(1, k+1) for k in k_array)), 1):
+        result = (result * i) // j
+    return result
+
 
 # k indexed in [0,n)
-def p_maxlh(n,k):
+def p_maxlh(n, k):
     """
     Calculates the maximum likelihood estimator for the kth sample
     of n total samples.
-    
+
     Args:
         n: the number of samples from the random variable, X
         k: the hierarchical position of the sample when ordered
@@ -17,15 +26,16 @@ def p_maxlh(n,k):
     """
     return (k+.5) / n
 
-def lhood(n,k,p=None):
+
+def lhood(n, k, p=None):
     """
     Calculates the likelihood that Prob(x < x_k) == p for the kth sample
     of n total samples.
-    
+
     Args:
         n: the number of samples from the random variable, X
         k: the hierarchical position of the sample when ordered
-            (k in [1,n])
+            (k in [0,n))
         p: the probability that X < Xk. Set to max. likelihood est. by default.
     Returns:
         the likelihood that Prob(x < x_k) == p
@@ -33,14 +43,15 @@ def lhood(n,k,p=None):
         N/A
     """
     if p is None:
-        p = p_maxlh(n,k)
+        p = p_maxlh(n, k)
 
-    return p**(k+.5) * (1-p)**(n-k-.5)
+    return nCkarray([k, n-k]) * p**(k+.5) * (1-p)**(n-k-.5)
 
-def d2dp2_lhood(n,k,p=None):
+
+def d2dp2_lhood(n, k, p=None):
     """
     Calculates the 2nd derivative of the likelihood function with respect to p.
-    
+
     Args:
         n: the number of samples from the random variable, X
         k: the hierarchical position of the sample when ordered
@@ -52,16 +63,21 @@ def d2dp2_lhood(n,k,p=None):
         N/A
     """
     if p is None:
-        p = p_maxlh(n,k)
+        p = p_maxlh(n, k)
 
-    return p**(k-1.5) * (1-p)**(n-k-2.5) * ((n-1)*p*(n*p - 2*k - 1)
-        + k**2 - .25)
+    return (
+        nCkarray([k, n-k])
+        * p**(k-1.5)
+        * (1-p)**(n-k-2.5)
+        * ((n-1)*p*(n*p - 2*k - 1) + k**2 - .25)
+    )
 
-def d2dp2_rlhood(n,k,p=None):
+
+def d2dp2_rlhood(n, k, p=None):
     """
-    Calculates the 2nd derivative of the max-unity-scaled likelihood function 
+    Calculates the 2nd derivative of the max-unity-scaled likelihood function
     with respect to p.
-    
+
     Args:
         n: the number of samples from the random variable, X
         k: the hierarchical position of the sample when ordered
@@ -72,10 +88,12 @@ def d2dp2_rlhood(n,k,p=None):
     Raises:
         N/A
     """
-    phat = p_maxlh(n,k)
+    phat = p_maxlh(n, k)
     if p is None:
         p = phat
 
-    return ((p/phat)**(k-1.5) * ((1-p)/(1-phat))**(n-k-2.5) / (phat*(1-phat))**2.
-        * ((n-1)*p*(n*p - 2*k - 1) + k**2 - .25))
-
+    return (
+        (p/phat)**(k-1.5)
+        * ((1-p)/(1-phat))**(n-k-2.5) / (phat*(1-phat))**2.
+        * ((n-1)*p*(n*p - 2*k - 1) + k**2 - .25)
+    )
