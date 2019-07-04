432dbfaf36808f3228c763499068a511694514c1
CrepeGoat <awqatty.b@gmail.com>
2019-07-04 11:48:27 -0400

    Added cdf_match module
    - added a function for comparing a set of samples from an unknown distribution against
    	a known distribution, & quantifying the likelihood that they are equivalent
    	via confidence interval
    - added one test for new function

---
+++ cdf_match/cdf_match.py
@@ -0,0 +1,22 @@
+import numpy as np
+import scipy.stats
+
+
+def cdf_match(X_cdf, x, presorted=False):
+    if presorted:
+        x = np.asarray(x)
+    else:
+        x = np.sort(x)
+    n = len(x)
+    i = np.arange(n)
+
+    confidence_levels = scipy.stats.binom.cdf(i, n, X_cdf(x))
+    confidence_levels = 2 * np.minimum(confidence_levels, 1-confidence_levels)
+
+    return np.power(2, np.sum(np.log2(confidence_levels)) / n)
+
+
+if __name__ == '__main__':
+    rv = scipy.stats.norm()
+    x = rv.rvs(100000)
+    print(cdf_match(rv.cdf, x))
---
+++ cdf_match/test_cdf_match.py
@@ -0,0 +1,43 @@
+import pytest
+from cdf_match import *
+
+import numpy as np
+import scipy.stats
+
+
+np.random.seed(0)
+
+
+def check_cdf_match(rv, cdf, sample_count):
+    confidence_level = 0.05
+    repetitions = 100
+
+    false_negatives = sum(
+        cdf_match(cdf, rv.rvs(sample_count)) < confidence_level
+        for _ in range(repetitions)
+    )
+
+    return false_negatives / repetitions < confidence_level
+
+
+@pytest.mark.parametrize('rv', [
+    scipy.stats.norm(),
+    scipy.stats.expon(),
+    scipy.stats.cauchy(),
+])
+def test_cdf_match_true_positives(rv):
+    assert check_cdf_match(rv, rv.cdf, sample_count=10**3)
+
+
+@pytest.mark.parametrize('rv_true, rv_guess', [
+    (scipy.stats.expon(), scipy.stats.norm(1, 1))
+])
+def test_cdf_match_false_positives(rv_true, rv_guess):
+    assert check_cdf_match(rv_true, rv_guess.cdf, sample_count=10)
+
+
+@pytest.mark.parametrize('rv_true, rv_guess', [
+    (scipy.stats.expon(), scipy.stats.norm(1, 1))
+])
+def test_cdf_match_true_negatives(rv_true, rv_guess):
+    assert not check_cdf_match(rv_true, rv_guess.cdf, sample_count=10**3)
